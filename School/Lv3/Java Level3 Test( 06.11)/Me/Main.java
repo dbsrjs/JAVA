4862219a32b407628e907908b9f0217f5019a26f
xyl_t9 <124150775+dbsrjs@users.noreply.github.com>
2024-06-12 21:50:11 +0900

    Create Main.java

---
+++ School/Lv3/Java Level3 Test( 06.11)/Me/Main.java
@@ -0,0 +1,29 @@
+package poly.sort;
+
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        //객체화 부분 작성1
+        Sort[] sort = {new BubbleSort(), new HeapSort(), new QuickSort()};
+
+        int[] nums = new int[5];
+
+        System.out.println("정렬 방식을 선택하세요.");
+        System.out.println("[1] : BubbleSort");
+        System.out.println("[2] : HeapSort");
+        System.out.println("[3] : QuickSort");
+
+        int choice = sc.nextInt();
+
+        selectedSort(sort[choice - 1], nums);
+    }
+
+    private static void selectedSort(Sort sort, int[] nums)
+    {
+        sort.ascending(nums);
+        sort.descending(nums);
+        sort.description();
+    }
+}
