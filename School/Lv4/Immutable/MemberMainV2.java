53c86bff87816ca5b95d404eeae36a9008c144d5
xyl_t9 <124150775+dbsrjs@users.noreply.github.com>
2024-06-18 17:04:57 +0900

    Create MemberMainV2.java

---
+++ School/Lv4/Immutable/MemberMainV2.java
@@ -0,0 +1,19 @@
+package lang.Immutable.address;
+
+public class MemberMainV2 {
+    public static void main(String[] args) {
+        ImmutableAddress address = new ImmutableAddress("서울");
+
+        MemberV2 memberA = new MemberV2("회원A", address);
+        MemberV2 memberB = new MemberV2("회원B", address);
+
+        System.out.println("memberA = " + memberA);
+        System.out.println("memberB = " + memberB);
+        
+        //memberB.getAddress().setValue("부산"); //컴파일 오류
+        memberB.setAddress(new ImmutableAddress("부산"));
+        System.out.println("부산 -> memberB.address");
+        System.out.println("memberA = " + memberA);
+        System.out.println("memberB = " + memberB);
+    }
+}
