f447832868201d60f37098b8dc5cc1f87d997f3e
xyl_t9 <124150775+dbsrjs@users.noreply.github.com>
2024-06-18 21:13:02 +0900

    Create StringSearchMain.java

---
+++ School/Lv4/string/StringSearchMain.java
@@ -0,0 +1,12 @@
+package lang.string;
+
+public class StringSearchMain {
+    public static void main(String[] args) {
+        String str = "Hello, Java! Welcome to Java World.";
+
+        System.out.println("문자열에 'Java'가 포함되어 있는지 : " + str.contains("Java"));//true
+        System.out.println("'Java'의 첫 번째 인덱스 : " + str.indexOf("Java"));//7
+        System.out.println("인덱스 10부터 'Java'의 인덱스 : " + str.indexOf("Java", 10));//24
+        System.out.println("'Java'의 마지막 인덱스 : " + str.lastIndexOf("Java"));//24
+    }
+}
