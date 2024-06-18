522f923e2463035fa76172dd9c93388fc8dd8e74
xyl_t9 <124150775+dbsrjs@users.noreply.github.com>
2024-06-18 17:04:37 +0900

    Create MemberV2.java

---
+++ School/Lv4/Immutable/MemberV2.java
@@ -0,0 +1,27 @@
+package lang.Immutable.address;
+
+public class MemberV2 {
+    private String name;
+    private ImmutableAddress address;
+
+    public MemberV2(String name, ImmutableAddress address) {
+        this.name = name;
+        this.address = address;
+    }
+
+    public ImmutableAddress getAddress() {
+        return address;
+    }
+
+    public void setAddress(ImmutableAddress address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "MemberV2{" +
+                "name='" + name + '\'' +
+                ", address=" + address +
+                '}';
+    }
+}
