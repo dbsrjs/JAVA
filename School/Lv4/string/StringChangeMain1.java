76e49725e67fb3471b92c78d679414116f36ec08
xyl_t9 <124150775+dbsrjs@users.noreply.github.com>
2024-06-19 18:36:48 +0900

    Create StringChangeMain1.java

---
+++ School/Lv4/string/StringChangeMain1.java
@@ -0,0 +1,13 @@
+package lang.string;
+
+public class StringChangeMain1 {
+    public static void main(String[] args) {
+        String str = "Hello, Java! Welcome to Java";
+
+        System.out.println("인덱스 7부터의 나머지 문자열 : " + str.substring(7));
+        System.out.println("인덱스 7부터 12까지의 부분 문자열 : " + str.substring(7, 12));
+        System.out.println("문자열 결합 : " + str.concat("!!!"));
+        System.out.println("'Java'를 'Worlod'로 대체 : " + str.replace("Java", "World"));
+        System.out.println("첫 번째 'Java'를 'World'로 대체 : " + str.replaceFirst("Java", "World"));
+    }
+}
