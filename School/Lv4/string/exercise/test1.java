a9be7c42876ffe1b8c01b891da70688a5fab6d61
xyl_t9 <124150775+dbsrjs@users.noreply.github.com>
2024-06-19 18:40:02 +0900

    Create test1.java

---
+++ School/Lv4/string/exercise/test1.java
@@ -0,0 +1,19 @@
+package lang.string.test;
+
+import java.util.Scanner;
+
+public class test1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.next();
+
+        for (int i = 0; i < str.length(); i++) {
+            String s = str.substring(i, i+1);
+            if(s.equals(s.toUpperCase()))//s가 s의 대문자와 가다면
+                System.out.print(s.toLowerCase());
+            else
+                System.out.print(s.toUpperCase());
+        }
+    }
+}
