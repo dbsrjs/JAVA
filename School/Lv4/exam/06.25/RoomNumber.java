7403b12b5da7d13c58ca78618719c5a7de8cdd9c
xyl_t9 <124150775+dbsrjs@users.noreply.github.com>
2024-06-25 17:39:05 +0900

    Create RoomNumber.java

---
+++ School/Lv4/exam/06.25/RoomNumber.java
@@ -0,0 +1,25 @@
+package Exam.ex0625;
+
+import java.util.Scanner;
+
+public class RoomNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int[] cnt = new int[10];
+        while(N > 0) {
+            cnt[N%10]++;
+            N = N/10;
+        }
+
+        int ans = (cnt[6] + cnt[9] + 1) / 2;
+        for (int i = 0; i < 9; i++) {
+            if(i != 6) {
+                ans = Math.max(ans, cnt[i]);
+            }
+        }
+
+        System.out.println(ans);
+    }
+}
