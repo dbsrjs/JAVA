2af265b32c18f1e3416ac45823546a729aa74185
xyl_t9 <124150775+dbsrjs@users.noreply.github.com>
2024-06-27 13:04:25 +0900

    Create PayMain1.java

---
+++ School/Lv3/Pay/1/PayMain1.java
@@ -0,0 +1,22 @@
+package poly.ex.pay1;
+
+public class PayMain1 {
+    public static void main(String[] args) {
+        PayService payService = new PayService();
+
+        //kakao 결제
+        String payOption1 = "Kakao";
+        int amount1 = 5000;
+        payService.processPay(payOption1, amount1);
+
+        //naver 결제
+        String payOption2 = "Naver";
+        int amount2 = 10000;
+        payService.processPay(payOption2, amount2);
+
+        //잘못된 결제 수단
+        String payOption3 = "apple";
+        int amount3 = 15000;
+        payService.processPay(payOption3, amount3);
+    }
+}
