8ffa63c1b301feb24a51995673f00e2a579fc017
xyl_t9 <124150775+dbsrjs@users.noreply.github.com>
2024-07-06 18:01:31 +0900

    Create ClassStart1.java

---
+++ School/Lv3/Class1(4.11)/Student/ClassStart1.java
@@ -0,0 +1,18 @@
+package Class1;
+
+public class ClassStart1 {
+
+	public static void main(String[] args) {
+		String stuName1 = "홍길동";
+		int stuAge1 = 15;
+		int stuGrade1 = 90;
+		
+		String stuName2 = "장발장";
+		int stuAge2 = 16;
+		int stuGrade2 = 80;
+		
+		System.out.println("이름:" + stuName1 + "나이: " + stuAge1 + "세 성적: " + stuGrade1 + "점");
+		System.out.println("이름:" + stuName2 + "나이: " + stuAge2 + "세 성적: " + stuGrade2 + "점");
+		
+	}
+}
