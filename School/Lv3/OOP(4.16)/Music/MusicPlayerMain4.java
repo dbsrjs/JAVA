a2cc088ff9e970351138f439b3f06bc1cff55138
xyl_t9 <124150775+dbsrjs@users.noreply.github.com>
2024-07-06 18:08:08 +0900

    Create MusicPlayerMain4.java

---
+++ School/Lv3/OOP(4.16)/Music/MusicPlayerMain4.java
@@ -0,0 +1,19 @@
+package OOP;
+
+public class MusicPlayerMain4 {
+	public static void main(String[] args) {
+		MusicPlayer player = new MusicPlayer();
+		
+		player.on();
+		
+		player.volumeUp();
+		
+		player.volumeUp();
+		
+		player.volumeDown();
+		
+		player.showStatus();
+		
+		player.off();
+	}
+}
