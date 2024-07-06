b70d3a7a494fa6613cba035effff432f77f507d4
xyl_t9 <124150775+dbsrjs@users.noreply.github.com>
2024-07-06 18:08:46 +0900

    Create RectangleMain.java

---
+++ School/Lv3/OOP(4.16)/Rectangle/RectangleMain.java
@@ -0,0 +1,14 @@
+package OOP.exam;
+
+public class RectangleMain {
+	public static void main(String[] args) {
+		Rectangle rec = new Rectangle();
+				
+		rec.width = 3;
+		rec.length = 3;
+		
+		rec.area();
+		rec.round();
+		rec.square();
+	}
+}
