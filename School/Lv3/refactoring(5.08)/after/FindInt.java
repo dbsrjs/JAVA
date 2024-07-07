494bc962fb1e13c0378846206e3b0d35443c082e
xyl_t9 <124150775+dbsrjs@users.noreply.github.com>
2024-07-07 14:26:02 +0900

    Update FindInt.java

--- School/Lv3/refactoring(5.08)/after/FindInt.java
+++ School/Lv3/refactoring(5.08)/after/FindInt.java
@@ -1,12 +1,16 @@
 package removecontrolflag.after;
 
 public class FindInt {
+
     public static boolean find(int[] data, int number) {
+
         boolean found = false;
 
-        for(int i = 0; i < data.length; i++) {
-            if(data[i] == number) {
+        for (int i = 0; i < data.length; i++) {
+
+            if (data[i] == number) {
                 found = true;
+                return found;
             }
         }
 
