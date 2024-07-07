82a8bc27b94b670cb26f6350559634072e608bf9
xyl_t9 <124150775+dbsrjs@users.noreply.github.com>
2024-07-07 14:09:49 +0900

    Create RobotMain.java

---
+++ School/Lv3/MagicNumber(5.07)/before/RobotMain.java
@@ -0,0 +1,11 @@
+package magicNumber.before;
+
+public class RobotMain {
+    public static void main(String[] args) {
+        Robot robot = new Robot("Smith");
+
+        robot.order(0);
+        robot.order(1);
+        robot.order(2);
+    }
+}
