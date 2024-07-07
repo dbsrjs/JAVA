c38348f4a38f034172c694c5d2fe74f7bcfc9f00
xyl_t9 <124150775+dbsrjs@users.noreply.github.com>
2024-07-07 14:15:07 +0900

    Create CarMain.java

---
+++ School/Lv3/Extends(5.07)/Overriding/CarMain.java
@@ -0,0 +1,17 @@
+package extends1.overriding;
+
+
+public class CarMain {
+    public static void main(String[] args) {
+
+        ElectricCar electricCar = new ElectricCar();
+        electricCar.move();
+        electricCar.charge();
+        electricCar.openDoor();
+
+        GasCar gasCar = new GasCar();
+        gasCar.move();
+        gasCar.fillUp();
+        gasCar.openDoor();
+    }
+}
