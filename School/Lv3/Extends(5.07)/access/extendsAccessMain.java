537d1dc90c2bc6db79c09cc58464f0007663497a
xyl_t9 <124150775+dbsrjs@users.noreply.github.com>
2024-07-07 14:11:40 +0900

    Create extendsAccessMain.java

---
+++ School/Lv3/Extends(5.07)/access/extendsAccessMain.java
@@ -0,0 +1,10 @@
+package extends1.access;
+
+import extends1.access.child.Child;
+
+public class extendsAccessMain {
+    public static void main(String[] args) {
+        Child chid = new Child();
+        chid.call();
+    }
+}
