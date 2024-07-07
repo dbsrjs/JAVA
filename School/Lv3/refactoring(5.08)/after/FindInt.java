dcd16e149f93f05472b02520770cb8a87e55dffc
xyl_t9 <124150775+dbsrjs@users.noreply.github.com>
2024-07-07 14:22:07 +0900

    Create FindInt.java

---
+++ School/Lv3/refactoring(5.08)/after/FindInt.java
@@ -0,0 +1,17 @@
+package removecontrolflag.after;
+
+public class FindInt {
+    public static boolean find(int[] data, int number) {
+        boolean found = false;
+
+        for(int i = 0; i < data.length; i++) {
+            if(data[i] == number) {
+                found = true;
+                //break;
+                return found;
+            }
+        }
+
+        return found;
+    }
+}
