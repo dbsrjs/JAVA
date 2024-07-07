4276e98431a94bb0f778f7ad484b093058ae78fe
xyl_t9 <124150775+dbsrjs@users.noreply.github.com>
2024-07-07 14:54:40 +0900

    Create Model3Car.java

---
+++ School/Lv3/Car(5.28)/car0/Model3Car.java
@@ -0,0 +1,15 @@
+package poly.car0;
+
+public class Model3Car{
+    public  void startEngine() {
+        System.out.println("K3Car.startEngine");
+    }
+
+    public void offEngine() {
+        System.out.println("K3Car.offEngine");
+    }
+
+    public void pressAccelerator() {
+        System.out.println("K3Car.pressAccelerator");
+    }
+}
