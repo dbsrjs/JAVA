5a7cbb280d6c50a0c282f560da92bb7e41a9d9c4
xyl_t9 <124150775+dbsrjs@users.noreply.github.com>
2024-07-07 14:12:48 +0900

    Create GasCar.java

---
+++ School/Lv3/Extends(5.07)/Ex1/GasCar.java
@@ -0,0 +1,11 @@
+package extends1.ex1;
+
+public class GasCar {
+    public void move() {
+        System.out.println("차를 이동합니다.");
+    }
+
+    public void fillUp() {
+        System.out.println("가스를 채웁니다.");
+    }
+}
