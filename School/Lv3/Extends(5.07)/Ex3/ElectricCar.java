b38ad06a1ec9c2a26243639aa89a7bcaffbccb99
xyl_t9 <124150775+dbsrjs@users.noreply.github.com>
2024-07-07 14:14:09 +0900

    Create ElectricCar.java

---
+++ School/Lv3/Extends(5.07)/Ex3/ElectricCar.java
@@ -0,0 +1,12 @@
+package extends1.ex3;
+
+public class ElectricCar extends Car {
+
+    /*
+    전기차 ElectricCar는 extends Car를 상ㅇ해서 부모클래스인 Car를 상속 받았다.
+    상속 덕분에
+     */
+    public void charge() {
+        System.out.println("충전합니다.");
+    }
+}
