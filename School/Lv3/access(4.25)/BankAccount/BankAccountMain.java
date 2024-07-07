fb99ef3f40a61ef1188f04a08da7ae31de704638
xyl_t9 <124150775+dbsrjs@users.noreply.github.com>
2024-07-07 14:05:34 +0900

    Create BankAccountMain.java

---
+++ School/Lv3/access(4.25)/BankAccount/BankAccountMain.java
@@ -0,0 +1,12 @@
+package access;
+
+public class BankAccountMain {
+    public static void main(String[] args) {
+        BankAccount account = new BankAccount();
+        account.deposit(10000);
+        account.withdraw(3000);
+        System.out.println("balance = " + account.getBalance());
+        account.deposit(-1999);
+        account.withdraw(10000);
+    }
+}
