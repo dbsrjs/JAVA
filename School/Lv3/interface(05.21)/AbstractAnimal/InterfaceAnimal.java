ccb9e95e66fa6fa36f3416d1f2f97a5be673c951
xyl_t9 <124150775+dbsrjs@users.noreply.github.com>
2024-07-07 14:48:36 +0900

    Create InterfaceAnimal.java

---
+++ School/Lv3/interface(05.21)/AbstractAnimal/InterfaceAnimal.java
@@ -0,0 +1,8 @@
+package poly.interface1;
+
+//인터페이스
+//인터페이스는 class 대신 interface 키워드를 사용하면 된다.
+public interface InterfaceAnimal {
+    public abstract void sound();
+    public abstract void move();
+}
