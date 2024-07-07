1f58aab27928ee5c254a1b6b6e9f6d4f1197903e
xyl_t9 <124150775+dbsrjs@users.noreply.github.com>
2024-07-07 14:54:29 +0900

    Create CarMain0.java

---
+++ School/Lv3/Car(5.28)/car0/CarMain0.java
@@ -0,0 +1,15 @@
+package poly.car0;
+
+public class CarMain0 {
+    public static void main(String[] args) {
+        Driver driver = new Driver();
+        K3Car k3car = new K3Car();
+        driver.setK3Car(k3car);
+        driver.drive();
+
+        Model3Car model3Car = new Model3Car();
+        driver.setK3Car(null);
+        driver.setModel3Car(model3Car);
+        driver.drive();
+    }
+}
