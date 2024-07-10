fe7a4ed4a1858029f56087aeca7c4474a7c5b074
xyl_t9 <124150775+dbsrjs@users.noreply.github.com>
2024-07-10 17:39:15 +0900

    Create WrapperTest1.java

---
+++ School/Lv4/wrapper/test/WrapperTest1.java
@@ -0,0 +1,12 @@
+package lang.wrapper.test;
+
+public class WrapperTest1 {
+    public static void main(String[] args) {
+        String str1 = "10";
+        String str2 = "20";
+
+        //문자로 입력된 str1과 str2의 합을 구하시오.
+
+        System.out.println(Integer.parseInt(str1) + Integer.parseInt(str2));
+    }
+}
