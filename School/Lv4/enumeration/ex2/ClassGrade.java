7af527bf8dcaf663ac5d3cedbbf0cb780084018b
xyl_t9 <124150775+dbsrjs@users.noreply.github.com>
2024-07-11 18:23:25 +0900

    Update ClassGrade.java

--- School/Lv4/enumeration/ex2/ClassGrade.java
+++ School/Lv4/enumeration/ex2/ClassGrade.java
@@ -4,6 +4,8 @@ public class ClassGrade {
     public static final ClassGrade BASIC = new ClassGrade();
     public static final ClassGrade GOLD = new ClassGrade();
     public static final ClassGrade DIAMOND = new ClassGrade();
+
+    private ClassGrade() {}
 }
 /*
 앞서 본 String처럼 아무 문자열이나 다 사용할 수 있는 것이 아니고, 우리가 나열한 항목들만
