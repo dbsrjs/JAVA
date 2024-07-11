00ddaadc24b513d29a81fc89126f1fc26a20bf0e
xyl_t9 <124150775+dbsrjs@users.noreply.github.com>
2024-07-11 18:30:24 +0900

    Create Grade.java

---
+++ School/Lv4/enumeration/ex03/Grade.java
@@ -0,0 +1,15 @@
+package enumeration.ex3;
+
+public enum Grade {
+    BASIC, GOLD, DIAMOND
+}
+//아래 코드와 같은 코드이다.
+/*
+public class ClassGrade {
+    public static final ClassGrade BASIC = new ClassGrade();
+    public static final ClassGrade GOLD = new ClassGrade();
+    public static final ClassGrade DIAMOND = new ClassGrade();
+
+    private ClassGrade() {}
+}
+ */
