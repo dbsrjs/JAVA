fc4c4ae5440845bbf6c32a754a6728be1ec4afce
xyl_t9 <124150775+dbsrjs@users.noreply.github.com>
2024-07-22 14:24:18 +0900

    Add files via upload

---
+++ School/Lv4/enumeration/test/ex1/AuthGradeMain2.java
@@ -0,0 +1,10 @@
+package enumeration.test.ex1;
+
+import java.util.Scanner;
+
+public class AuthGradeMain2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("당신의 등급을 입력하세요");
+    }
+}
