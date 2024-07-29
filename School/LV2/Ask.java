7ab5461c930efa9777fbb99110ae31a89bba30c8
xyl_t9 <124150775+dbsrjs@users.noreply.github.com>
2024-07-29 23:49:57 +0900

    Create Ask.java

---
+++ School/LV2/Ask.java
@@ -0,0 +1,17 @@
+package jv_0712;
+
+public class Ask {
+    public static void main(String[] args) {
+	       
+        char a = 'S';
+        char b = 'U';
+        char c = 'N';
+        char d = 'G';
+        char e = 'I';
+        char f = 'L';
+       
+        int sum = a+b+c+d+e+f;
+       
+        System.out.println(sum);        
+    }
+}
