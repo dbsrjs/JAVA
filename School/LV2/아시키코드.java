f77841ef1bc5589f33c50baa6f7a18fd890945bd
xyl_t9 <124150775+dbsrjs@users.noreply.github.com>
2024-07-30 00:17:19 +0900

    Create 아시키코드.java

---
+++ School/LV2/아시키코드.java
@@ -0,0 +1,16 @@
+public class ask 
+{
+	public static void main(String[] args) 
+	{
+		char a= 'S';
+		char b= 'U';
+		char c= 'N';
+		char d= 'G';
+		char e= 'I';
+		char f= 'L';
+		
+		int sum = a+b+c+d+e+f;
+		
+		System.out.println(sum);
+	}
+}
