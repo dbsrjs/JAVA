9a7ef1530646eecfba222757dc5a0784ceecb34b
xyl_t9 <124150775+dbsrjs@users.noreply.github.com>
2024-07-29 23:50:41 +0900

    Create Poketmon.java

---
+++ School/LV2/Poketmon/Poketmon.java
@@ -0,0 +1,17 @@
+
+public class Poketmon {
+//	     - 속성 : HP, 공격력, 방어력, 레벨
+	    String name;
+	    int hp;
+	    int atk;
+	    int def;
+	    int level = 1;
+	   
+//	     - 기능 : 울기, 레벨업
+	    void 울기() {
+	    }
+	   
+	    void 레벨업() {
+	        level++;
+	    }
+    }
