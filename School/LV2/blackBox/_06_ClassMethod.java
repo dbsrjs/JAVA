f58ef4e6b981ee3dc143b0fee276ce1c62bff1cd
xyl_t9 <124150775+dbsrjs@users.noreply.github.com>
2024-08-22 01:17:40 +0900

    Create _06_ClassMethod.java

---
+++ School/LV2/blackBox/_06_ClassMethod.java
@@ -0,0 +1,10 @@
+package blackBox;
+
+public class _06_ClassMethod {
+    public static void main(String[] args) {
+        BlackBox b1 = new BlackBox();
+        b1.modelName = "까망이";
+
+        BlackBox.callServiceCenter();
+    }
+}
