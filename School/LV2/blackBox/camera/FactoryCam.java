fe998121c03c2dfadcaf02ecb16d3c0c7d60a3eb
xyl_t9 <124150775+dbsrjs@users.noreply.github.com>
2024-08-22 01:14:30 +0900

    Create FactoryCam.java

---
+++ School/LV2/blackBox/camera/FactoryCam.java
@@ -0,0 +1,17 @@
+package blackBox.camera;
+
+public class FactoryCam extends Camera {   //자식 클래스
+
+    public FactoryCam() {
+        this.name = "공장 카메라";
+    }
+
+    public void detectFire() {
+        System.out.println("화재를 감지합니다.");
+    }
+
+    @Override
+    public void showMainFeature() {
+        System.out.println(this.name + "의 주요 기능 : 화재 감지");
+    }
+}
