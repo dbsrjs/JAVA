701360c2d31a0f2804a3902c0771dabd471a85b5
xyl_t9 <124150775+dbsrjs@users.noreply.github.com>
2024-08-22 01:16:35 +0900

    Create _01_Class.java

---
+++ School/LV2/blackBox/_01_Class.java
@@ -0,0 +1,25 @@
+package blackBox;
+
+public class _01_Class {
+    public static void main(String[] args) {
+        //객체 지향 프로그래밍(OOP : Object - Oriented - Programming)
+        //유지보수 용이
+        //재사용성이 높음
+        
+        //차량용 블랙박스
+        //모델명, 해상도, 가격, 색상
+
+        String modelName = "까망이";
+        String resolution = "FHD";
+        int price = 200000;
+        String color = "블랙";
+
+        //새로운 제품을 개발한다면
+        String modelName2 = "하양이";
+        String resolution2 = "UHD";
+        int price2 = 300000;
+        String color2 = "화이트";
+
+        //또 새로운 제품을 개발한다면..?
+    }
+}
